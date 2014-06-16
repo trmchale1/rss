daeb62b37a8f9bdd9c5777f49c1855b72f3ccd12
trmchale1 <trmchale@gmail.com>
2014-06-15 23:00:33 -0400

    nyt and apple done

--- ARSSReader/MasterViewController.m
+++ ARSSReader/MasterViewController.m
@@ -14,7 +14,9 @@
 @interface MasterViewController ()
 {
     NSMutableArray *_objects;
+    NSMutableArray *_nytObjects;
     NSURL* feedURL;
+    NSURL* twoURL;
     UIRefreshControl* refreshControl;
 }
 @end
@@ -28,6 +30,7 @@
     //configuration
     self.title = @"Tim's RSS Reader";
     feedURL = [NSURL URLWithString:@"http://images.apple.com/main/rss/hotnews/hotnews.rss"];
+    twoURL = [NSURL URLWithString:@"http://rss.nytimes.com/services/xml/rss/nyt/HomePage.xml"];
     
     //add refresh control to the table view
     refreshControl = [[UIRefreshControl alloc] init];
@@ -37,6 +40,7 @@
              forControlEvents:UIControlEventValueChanged];
     
     NSString* fetchMessage = [NSString stringWithFormat:@"Fetching: %@",feedURL];
+    NSString* twoFetch = [NSString stringWithFormat:@"Fetching: %@",twoURL];
     
     refreshControl.attributedTitle = [[NSAttributedString alloc] initWithString:fetchMessage
                                                                      attributes:@{NSFontAttributeName:[UIFont fontWithName:@"Helvetica" size:11.0]}];
@@ -64,24 +68,59 @@
                     dispatch_async(dispatch_get_main_queue(), ^{
                         
                         //UI code on the main queue
-                        [(TableHeaderView*)self.tableView.tableHeaderView setText:title];
+                     //   [(TableHeaderView*)self.tableView.tableHeaderView setText:title];
                         
                         _objects = results;
                        // NSLog(@"_objects = %@", _objects);
-                        NSMutableIndexSet *indexes = [NSMutableIndexSet indexSetWithIndex:1];
-                        [indexes addIndex:0];
-                        [indexes addIndex:1];
-                        [indexes addIndex:2];
-                        [indexes addIndex:3];
-                        
-                        [_objects removeObjectsInRange:(NSRange){6,14}];
-                        [_objects removeObjectsAtIndexes:indexes];
+//                        NSMutableIndexSet *indexes = [NSMutableIndexSet indexSetWithIndex:1];
+//                        [indexes addIndex:0];
+//                        [indexes addIndex:1];
+//                        [indexes addIndex:2];
+//                        [indexes addIndex:3];
+//                        
+//                        [_objects removeObjectsInRange:(NSRange){6,14}];
+//                        [_objects removeObjectsAtIndexes:indexes];
                         [self.tableView reloadData];
                         
                         // Stop refresh control
                         [refreshControl endRefreshing];
                     });
                 }];
+    
+    RSSLoader* rssTwo = [[RSSLoader alloc] init];
+    [rssTwo fetchRssWithURL:twoURL
+                complete:^(NSString *NYTtitle, NSArray *nytResults) {
+                    
+                    //completed fetching the RSS
+                    dispatch_async(dispatch_get_main_queue(), ^{
+                        
+                        //UI code on the main queue
+                    //    [(TableHeaderView*)self.tableView.tableHeaderView setText:title];
+                     //   NSIndexSet *indexSet = [NSIndexSet indexSetWithIndexesInRange:NSMakeRange(2, 25)];
+                        _nytObjects = nytResults;
+
+                        [_objects addObjectsFromArray:nytResults];
+                        NSLog(@"a = %@", _objects);
+                        
+                        //     NSLog(@"x = %@", _objects);
+                      //  [_objects insertObjects:nytResults atIndexes:indexSet];   THIS MIGHT BE USEFUL
+                     //   NSLog(@"_objects = %@", _objects);
+                     //    NSLog(@"_objects = %@", _nytObjects);
+//                        NSMutableIndexSet *indexes = [NSMutableIndexSet indexSetWithIndex:1];
+//                        [indexes addIndex:0];
+//                        [indexes addIndex:1];
+//                        [indexes addIndex:2];
+//                        [indexes addIndex:3];
+//                        
+//                        [_objects removeObjectsInRange:(NSRange){6,14}];
+//                        [_objects removeObjectsAtIndexes:indexes];
+                        [self.tableView reloadData];
+                        
+                        // Stop refresh control
+                        [refreshControl endRefreshing];
+                    });
+                }];
+
     
 }
 
@@ -92,13 +131,17 @@
     return 1;
 }
 
+// So return x when you figure out which array is out of bounds
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-    return _objects.count;
+    long x;
+    x = _objects.count;
+    return x;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
+    
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Cell" forIndexPath:indexPath];
 
     RSSItem *object = _objects[indexPath.row];
